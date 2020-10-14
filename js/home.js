59251183cfbbc90e88006f7fc3d4e0982ba24731
Matthew Foley <matthewfoley99@gmail.com>
2020-10-14 17:12:00 -0400

    Home done 4 now. Mobile friendly navbar.

--- js/home.js
+++ js/home.js
@@ -1,54 +1,59 @@
+window.addEventListener('load', animateWall)
+
 // creates dynamic keyframes for infinity banner based on number of images included in each row
-var imagesTop = document.getElementById('top').childNodes;
-var imagesBot= document.getElementById('bottom').childNodes;
-var bannerLengthTop = 0;
-var bannerLengthBot = 0;
+function animateWall () {
+    console.log("Got this far.");
+    var imagesTop = document.getElementById('top').childNodes;
+    var imagesBot= document.getElementById('bottom').childNodes;
+    var bannerLengthTop = 0;
+    var bannerLengthBot = 0;
 
-for(let i=0; i < (imagesTop.length/2); i++) {
-    if (imagesTop[i].nodeName.toLowerCase() == 'img') {
-        console.log(imagesTop[i].src + ' ' + imagesTop[i].clientWidth);
-        bannerLengthTop -= (imagesTop[i].clientWidth);
+    for(let i=0; i < (imagesTop.length/2); i++) {
+        if (imagesTop[i].nodeName.toLowerCase() == 'img') {
+            console.log(imagesTop[i].src + ' ' + imagesTop[i].clientWidth);
+            bannerLengthTop -= (imagesTop[i].clientWidth);
+        }
     }
-}
 
-for(let i=0; i < (imagesBot.length/2); i++) {
-    if (imagesBot[i].nodeName.toLowerCase() == 'img') {
-        console.log(imagesBot[i].clientWidth);
-        bannerLengthBot -= (imagesBot[i].clientWidth);
+    for(let i=0; i < (imagesBot.length/2); i++) {
+        if (imagesBot[i].nodeName.toLowerCase() == 'img') {
+            console.log(imagesBot[i].clientWidth);
+            bannerLengthBot -= (imagesBot[i].clientWidth);
+        }
     }
+
+    var styleTop = document.createElement('style');
+    var styleBot = document.createElement('style');
+
+    var keyFramesTop = '\
+    @keyframes infinityBannerTop {\
+        0% {\
+            transform: translateX(0);\
+        }\
+        100% {\
+            transform: translateX(TRANSLATE_DISTANCE);\
+        }\
+    }\'';
+
+    var keyFramesBot = '\
+    @keyframes infinityBannerBot {\
+        0% {\
+            transform: translateX(0);\
+        }\
+        100% {\
+            transform: translateX(TRANSLATE_DISTANCE);\
+        }\
+    }\'';
+
+    keyFramesTop = keyFramesTop.replace(/TRANSLATE_DISTANCE/g, bannerLengthTop + "px");
+    keyFramesBot = keyFramesBot.replace(/TRANSLATE_DISTANCE/g, bannerLengthBot + "px");
+
+
+    styleTop.innerHTML = keyFramesTop;
+    styleBot.innerHTML = keyFramesBot;
+    document.getElementsByTagName('head')[0].appendChild(styleTop);
+    document.getElementsByTagName('head')[0].appendChild(styleBot);
+
+    console.log(keyFramesTop);
+    console.log(keyFramesBot);
 }
-
-var styleTop = document.createElement('style');
-var styleBot = document.createElement('style');
-
-var keyFramesTop = '\
-@keyframes infinityBannerTop {\
-    0% {\
-        transform: translateX(0);\
-    }\
-    100% {\
-        transform: translateX(TRANSLATE_DISTANCE);\
-    }\
-}\'';
-
-var keyFramesBot = '\
-@keyframes infinityBannerBot {\
-    0% {\
-        transform: translateX(0);\
-    }\
-    100% {\
-        transform: translateX(TRANSLATE_DISTANCE);\
-    }\
-}\'';
-
-keyFramesTop = keyFramesTop.replace(/TRANSLATE_DISTANCE/g, bannerLengthTop + "px");
-keyFramesBot = keyFramesBot.replace(/TRANSLATE_DISTANCE/g, bannerLengthBot + "px");
-
-
-styleTop.innerHTML = keyFramesTop;
-styleBot.innerHTML = keyFramesBot;
-document.getElementsByTagName('head')[0].appendChild(styleTop);
-document.getElementsByTagName('head')[0].appendChild(styleBot);
-
-console.log(keyFramesTop);
-console.log(keyFramesBot);
---
+++ js/navlinks.js
@@ -0,0 +1,13 @@
+const navigation = document.getElementById('navigation');
+const menuButton = document.getElementById('menu-button');
+
+menuButton.addEventListener('click', () => {
+    console.log('menu');
+    navigation.classList.toggle('open');
+    event.stopPropagation();
+});
+
+document.addEventListener('click', () => {
+    navigation.classList.remove('open');
+});
+
